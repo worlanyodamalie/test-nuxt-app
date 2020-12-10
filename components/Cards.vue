7b262d42fc50984c955f22d52879724c9ca9fd1f
Worlanyo <worlanyo@farmerline.co>
2020-12-10 18:45:57 +0000

    Make card componenet dynamic

--- components/Cards.vue
+++ components/Cards.vue
@@ -1,18 +1,32 @@
 <template>
-    <div class="flex justify-center">
-        <article class="mw5 pa3 ma4 br3 bg-gray washed-blue">
+    <div class="flex flex-wrap justify-center">
+        <article v-for="cardItem in cardItems" :key="cardItem.title" v-tooltip="'This is a card'" class="mw5 pa3 ma4 br3 bg-gray washed-blue">
             <div class="tc">
-                <img src="http://tachyons.io/img/avatar_1.jpg" class="br-100 h4 w4 dib ba b--black-05 pa2" title="Photo of a kitty staring at you">
-                <h1 class="f3">Cat</h1>
-                <h1 class="f5">The cat is a domestic species of small carnivorous mammal. It is the only domesticated species in the family Felidae and is often referred to as the domestic cat to distinguish it from the wild members of the family.</h1>
+                <img :src="cardItem.image" class="h4 w4 dib ba b--black-05 pa2"  :class="[cardItem.title === 'Cat' ? 'br-100' :  '']">
+                <h1 class="f3">{{cardItem.title}}</h1>
+                <h1 class="f5">{{cardItem.description}}</h1>
             </div>
-        </article> 
-        <article class="mw5 pa3 ma4 br3 bg-gray washed-blue">
-            <div class="tc">
-                <img src="http://tachyons.io/img/logo.jpg" class="h4 w4 dib ba b--black-05 pa2">
-                <h1 class="f3">Rounded Medium</h1>
-                <h1 class="f5">This image has rounded edges</h1>
-            </div>
-        </article>
+        </article>    
     </div>
 </template>
+<script>
+export default {
+    data(){
+        return {
+           cardItems: [
+               {
+                   image: 'http://tachyons.io/img/avatar_1.jpg',
+                   title: 'Cat',
+                   description: 'The cat is a domestic species of small carnivorous mammal. It is the only domesticated species in the family Felidae and is often referred to as the domestic cat to distinguish it from the wild members of the family.'
+               },
+               {
+                   image: 'http://tachyons.io/img/logo.jpg',
+                   title: 'Rounded Medium',
+                   description: 'This image has rounded edges.'
+               }
+
+           ]
+        }
+    }
+}
+</script>
